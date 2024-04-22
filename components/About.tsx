fbc70f8ad18c6813d00acc7a43e587225f8acacc
Aradhya <aradhya@my.yorku.ca>
2024-04-22 00:02:08 -0400

    2

--- components/About.tsx
+++ components/About.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { Card, CardDescription } from './ui/card'
+import { Card, CardDescription, CardHeader } from './ui/card'
 import { HoverEffect } from './ui/card-hover-effect'
 import { title } from 'process'
 
 function About() {
     return (
-       <div className=''>
-            <HoverEffect items={about} />
-       </div>
+       <Card>
+            <CardHeader>
+                <CardDescription>As a programmer extraordinaire, I juggle two roles: slaying bugs as an intern QA Developer at theScore and weaving code magic as a Software Developer at Fibra, Software development is my playground, and I\'m always hungry for new challenges to devour. Let\'s team up and code the world into a better place! </CardDescription>
+            </CardHeader>
+       </Card>
     )
 }
 
 export const about = [
     {
         title: '',
-        description: 'As a programmer extraordinaire, I juggle two roles: slaying bugs as an intern QA Developer at theScore and weaving code magic as a Software Developer at Fibra, Software development is my playground, and I\'m always hungry for new challenges to devour. Let\'s team up and code the world into a better place! 🚀',
+        description: '🚀',
         link: '',
     }
 ]
