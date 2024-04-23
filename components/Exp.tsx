9b0e13c968df4a14c5b26c2eaf6de94bb8a4bef5
Aradhya <aradhya@my.yorku.ca>
2024-04-23 13:19:29 -0400

    Card

--- components/Exp.tsx
+++ components/Exp.tsx
@@ -3,15 +3,26 @@ import { Card, CardTitle, CardHeader, CardDescription } from './ui/card'
 
 function Exp() {
   return (
-   <div>
-      <div className='group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
-        <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'>
-          <div className='z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2'>2023-2023</div>
-          <div className='z-10 sm:col-span-6'>Time</div>
+    <div className='group relative grid pb-1 transition-all sm:grid-cols-9 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
+      <div className='mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-3'>Jan 2023-Sept 2023</div>
+      <div className='sm:col-span-6'>
+        <h3 className='font-medium leading-snug text-slate-200'>
+          <div>
+            <a className='inline-flex items-baseline font-medium leading-tight text-slate-200 '>
+              <span className='-inset-x-4 -inset-y-2.5 md:-inset-x-6 md:-inset-y-4 lg:block'>QA Analyst · </span>
+              
+              <span className='inline-block'>theScore</span>
+            </a>
+          </div>
+        </h3>
+        <p className='mt-2 text-sm leading-normal'>Description</p>
+        <ul className='mt-2 flex flex-wrap'>
+          <li className='mr-1.5 mt-2'>
+            <div className='flex items-center rounded-full bg-white/40 px-3 py-1 text-xs font-medium leading-5 text-slate-100'>Tools</div>
+          </li>
+        </ul>
       </div>
     </div>
- 
-   </div>
   )
 }
 
