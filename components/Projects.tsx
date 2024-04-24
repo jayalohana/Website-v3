b4995417a931c09ab3886fa8b526470446d48dec
Aradhya <aradhya@my.yorku.ca>
2024-04-24 13:21:42 -0400

    ProjectFix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -71,7 +71,7 @@ const Projects = () => {
                 </p>
                 <ul className='mt-2 flex flex-wrap'>
               {project.tools.map((tool, toolIndex) => (
-                <li className='mr-1.5 mt-2'>
+                <li className='mr-1.5 mt-2' key={toolIndex}>
                   <div className='flex items-center rounded-full bg-white/40 px-3 py-1 text-xs font-medium leading-5 text-slate-100'>{tool}</div>
                 </li>
               ))}
